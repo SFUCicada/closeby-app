eb573dd3cdf3bfd310c2d09ed504586e9c7b7ad9
Shannon Schmitt <83383502+SFUCicada@users.noreply.github.com>
2023-02-22 15:35:05 -0500

    new sliders

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import logo from './assets/images/white-logo.png';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 
 import './index.css';
-import Slider from './slider';
+import Slider2 from './slider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Slider />
+    <Slider2 />
   </React.StrictMode>
 );
