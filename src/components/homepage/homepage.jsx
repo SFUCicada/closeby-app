f509f835b562b5f50a7a5373cee6705779178172
samwarnerr <warnersam20@gmail.com>
2023-02-23 15:28:35 -0500

    added go button

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 import { Link } from 'react-router-dom';
 
 import { Slider, Tags } from '..';
@@ -7,6 +10,14 @@ import logo from "../../images/white-logo.png";
 
 
 import "./homepage.css";
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
 
 
 const homepage = () => {
@@ -16,6 +27,7 @@ const homepage = () => {
           <header className="app-header" />
             <Slider />
             <Tags />
+            <ColorButton variant="Contained">go!</ColorButton>
         </body>
       </div>
     );
