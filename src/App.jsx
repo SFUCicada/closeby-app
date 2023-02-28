e99d614b2316a54dfd69b754d2dbcafc4843ce54
Ethan Walker <ethanwalker23081@gmail.com>
2023-02-28 13:54:29 -0500

    put back to normal lol my b

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Homepage, Slider, Navbar, About, Support } from './components';
-import handleSubmit from './handlers/handlesubmit';
+//import handleSubmit from './handlers/handlesubmit';
 
 const App = () => {
-  const submithandler = (e) => {
-    e.preventDefault()
-    handleSubmit(dataRef.current.value)
-    dataRef.current.value = ""
-  }
+  // const submithandler = (e) => {
+  //   e.preventDefault()
+  //   handleSubmit(dataRef.current.value)
+  //   dataRef.current.value = ""
+  // }
   return (
     <Router>
       <div>
